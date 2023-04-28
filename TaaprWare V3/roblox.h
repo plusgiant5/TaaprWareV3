610dc439bf3e00d985253e272b39497a71675fc4
plusgiant5 <102703670+plusgiant5@users.noreply.github.com>
2023-04-27 21:32:12 -0700

    Update 7

--- TaaprWare V3/roblox.h
+++ TaaprWare V3/roblox.h
@@ -13,28 +13,28 @@ const std::string dummy_bytecode = "\x1B\x7B\x56\x24\xA3\xCC\xB8\xB9\xB9\xC5\x73
 const uintptr_t base = reinterpret_cast<uintptr_t>(GetModuleHandle(NULL));
 
 namespace addresses {
-	const uintptr_t getscheduler = base + 0x7320B0;
-	const uintptr_t task_defer = base + 0x3DC560; // This could be any function that pops a function from the Luau stack and calls it. Defer is one of them. Also note that task.defer silently logs suspicious calls
-	const uintptr_t luavm_load = base + 0x356B40;
-	const uintptr_t luavm_load_bytecode_hook = base + 0x359CF0;
-	const uintptr_t luavm_load_hashcheck_hook = base + 0x359CC4;
+	const uintptr_t getscheduler = base + 0x738CC0;
+	const uintptr_t task_defer = base + 0x3DFF20; // This could be any function that pops a function from the Luau stack and calls it. Defer is one of them. Also note that task.defer silently logs suspicious calls
+	const uintptr_t luavm_load = base + 0x359BA0;
+	const uintptr_t luavm_load_bytecode_hook = base + 0x35CD14;
+	const uintptr_t luavm_load_hashcheck_hook = base + 0x35CCE8;
 }
 
 namespace offsets {
 	namespace scriptcontext {
 		constexpr uintptr_t get_scriptstate(uintptr_t scriptcontext) {
-			return *(uintptr_t*)(scriptcontext + 0xF4) ^ (scriptcontext + 0xF4); // Every encryption changes every week
+			return *(uintptr_t*)(scriptcontext + 0xF4) - (scriptcontext + 0xF4); // Every encryption changes every week
 		}
 	}
 	namespace state {
-		constexpr int top = 0xC; // Luau offsets change every week
+		constexpr int top = 0x8; // Luau offsets change every week
 	}
 	// These only change when roblox makes changes to luavm_load
 	// Find them using your disassembler
 	namespace luavm_load_stackframe {
 		// Because I can't type the whole namespace paths in inline assembly
-		#define offsets__luavm_load_stackframe__bytecode -0x164
-		#define offsets__luavm_load_stackframe__bytecode_len -0x5C
+		#define offsets__luavm_load_stackframe__bytecode -0x15C
+		#define offsets__luavm_load_stackframe__bytecode_len -0x6C
 	}
 }
 
